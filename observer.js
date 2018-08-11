91ee9a27b77d64b889446781f1e17593f34eec20
Animesh Kahara <animeshcyberphysco@gmail.com>
2018-08-11 18:50:06 +0530

    -Added Observer with boundary and corner control. -Random Motion

---
+++ observer.js
@@ -0,0 +1,41 @@
+class Observer {
+
+
+  constructor(x, y)
+  {
+    this.x = width/2;
+    this.y = height/2;
+  }
+
+  show()
+  {
+    stroke(255,0,0);
+    strokeWeight(15);
+    point(this.x,this.y);
+  }
+
+  move(dir)
+  {
+
+    switch(dir){
+      case 1:
+              this.y -=  gh;
+              if(this.y < gh) { this.y = gh; }
+              break;
+      case 2:
+              this.x +=  gw;
+              if(this.x > width-gw) { this.x = width-gw; }
+              break;
+      case 3:
+              this.y +=  gh;
+              if(this.y > height-gh) { this.y = height-gh; }
+              break;
+
+      case 4:
+              this.x -=  gw;
+              if(this.x < gw) { this.x = gw; }
+              break;
+      }
+  }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
+let gw; //Grid Width
+let gh; //Grid Height
+let a;
+
 function setup(){
   createCanvas(600,600);
   background(255);
-}
+  frameRate(5);
+  gw = width / 10;
+  gh = height / 10;
+  a = new Observer;
+ }
 
 function draw(){
+  background(255);
   drawGrid();
+  a.show();
+  a.move(floor(random(1,5)));
 }
 
 function drawGrid(){
-  for(var i=60;i<600;i+=60)
+  for(var i=gw;i<width;i+=gw)
   {
     strokeWeight(2);
-    line(i,60,i,height - 60);
-    line(60,i,width - 60,i);
+    stroke(0);
+    line(i,gh,i,height - gh);
+    line(gw,i,width - gw,i);
   }
 }
+
+// function mousePressed(){
+//   a.move(floor(random(1,5)));
+// }
